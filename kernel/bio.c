6966c8e0b77fc110b7f072bb6b88b3da1567e8ae
Cccj <xcj_19857370076@outlook.com>
2025-04-03 15:11:09 +0800

    using hashmap to add locks to cache

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,69 +23,144 @@
 #include "fs.h"
 #include "buf.h"
 
-struct {
-  struct spinlock lock;
-  struct buf buf[NBUF];
+// 哈希表的桶号索引.设置个数为质数的桶数可以降低哈希冲突
+#define NBUFMAP_BUCKET 13
+// 哈希桶索引计算 (key 值)
+#define BUFMAP_HASH(dev, blockno) ((((dev) << 27) | (blockno)) % NBUFMAP_BUCKET)
 
-  // Linked list of all buffers, through prev/next.
-  // Sorted by how recently the buffer was used.
-  // head.next is most recent, head.prev is least.
-  struct buf head;
+struct {
+  // struct spinlock lock;
+  struct buf buf[NBUF];
+  
+  // 将双链表改为哈希表
+  struct buf bufmap[NBUFMAP_BUCKET];
+  struct spinlock bufmap_locks[NBUFMAP_BUCKET];
+
+  // 添加驱逐锁
+  struct spinlock eviction_lock;
+
 } bcache;
 
 void
 binit(void)
 {
-  struct buf *b;
+  // 初始化桶锁
+  for(int i = 0; i < NBUFMAP_BUCKET; i++){
+    initlock(&bcache.bufmap_locks[i], "bcache_bufmap");
+    bcache.bufmap[i].next = 0;
+  }
 
-  initlock(&bcache.lock, "bcache");
-
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+  // 初始化缓冲区块，初始时均连接到桶号索引为 0 的哈希桶中
+  for (int i = 0; i < NBUF; i++)
+  {
+    struct buf *b = &bcache.buf[i];
+    initsleeplock(&b->lock,"buffer");
+    b->lastuse = 0;
+    b->refcnt = 0;
+    
+    // 所有缓冲区块添加到哈希缓冲 bufmap[0] 中
+    b->next = bcache.bufmap[0].next;
+    bcache.bufmap[0].next = b;
   }
 }
 
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
+// 从缓冲区缓存中获取磁盘块.
+// 若该磁盘块已经在缓存中,就增加其引用计数并返回;若没有缓存该块,则选择一个空闲的缓冲区与指定的磁盘块关联
 static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
 
-  acquire(&bcache.lock);
+  // 哈希计算获取桶号
+  uint key = BUFMAP_HASH(dev,blockno);
 
-  // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  // 哈希桶上锁
+  acquire(&bcache.bufmap_locks[key]);
+
+  // 查找缓冲区块
+  // 若在缓冲区中
+  for(b = bcache.bufmap[key].next;b;b = b->next){
+    if(b->dev == dev && b->blockno == blockno){
+      b->refcnt ++;
+      release(&bcache.bufmap_locks[key]);
+      acquiresleep(&b->lock);     // 表示其他线程不能再修改这个缓冲区了
+      return b;
+    }
+  }
+
+  // 若缓冲区未命中,则先释放哈希桶锁,再获取驱逐锁
+  release(&bcache.bufmap_locks[key]);
+  acquire(&bcache.eviction_lock);
+
+  // 在释放桶锁到获取驱逐锁的间隙可能会有别的线程创建了需要的缓冲区块,所以要再检查一次,避免重复创建缓冲区块
+  for(b = bcache.bufmap[key].next; b; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+      release(&bcache.bufmap_locks[key]);
       acquiresleep(&b->lock);
-      return b;
     }
   }
 
-  // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->valid = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+  // 如果在这间隙缓冲区还是没有命中，则通过 LRU 算法进行替换缓冲区
+  struct buf* before_least = 0;   // 记录要替换的前一个块
+  uint holding_bucket = -1;     // 记录当前持有哪个桶锁
+  
+  // 循环所有桶查找缓冲区进行替换
+  for(int i = 0;i  < NBUFMAP_BUCKET; i++){
+    acquire(&bcache.bufmap_locks[i]);   // 给当前桶上锁
+
+    int newfound = 0;   // 是否在当前桶找到新的可以替换的缓冲区
+    
+    // 遍历当前桶查找可以替换的缓冲区
+    for(b = &bcache.bufmap[i]; b; b = b->next){
+      if(b->next->refcnt == 0 && (!before_least || b->next->lastuse < before_least->lastuse)){
+        before_least = b;
+        newfound = 1;
+      }
+    }
+
+    // 当前桶没找到
+    if(!newfound)
+      release(&bcache.bufmap_locks[i]);
+    else{
+      if(holding_bucket != -1)   // 如果不为 -1 ,说明之前持有桶锁,需要释放
+        release(&bcache.bufmap_locks[holding_bucket]);
+      holding_bucket = i;
     }
   }
-  panic("bget: no buffers");
+
+  // 没找到可以替换的缓冲区
+  if(!before_least)
+    panic("bget: no buffers");
+
+  b = before_least->next;
+
+  // 如果要替换的桶不在 key 桶中，就要把那块缓冲区从其所在桶分离出来
+  if(holding_bucket != key){
+    before_least->next = b->next;
+    release(&bcache.bufmap_locks[holding_bucket]);    // 要分离的缓冲区所在桶解锁
+    
+    // 要加入的桶上锁
+    acquire(&bcache.bufmap_locks[key]);
+    b->next = bcache.bufmap[key].next;
+    bcache.bufmap[key].next = b;
+  }
+
+  // 设置新 buf 的字段
+  b->dev = dev;
+  b->blockno = blockno;
+  b->refcnt = 1;
+  b->valid = 0;
+
+  // 释放相关锁
+  release(&bcache.bufmap_locks[key]);
+  release(&bcache.eviction_lock);
+  acquiresleep(&b->lock);
+
+  return b;
 }
 
 // Return a locked buf with the contents of the indicated block.
@@ -121,33 +196,31 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  uint key = BUFMAP_HASH(b->dev, b->blockno);
+  acquire(&bcache.bufmap_locks[key]);
   b->refcnt--;
   if (b->refcnt == 0) {
-    // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    // 如果缓冲区的引用计数降为 0，则更新缓冲区的最后使用时间 lastuse 为当前的系统时间 ticks
+    b->lastuse = ticks;
   }
   
-  release(&bcache.lock);
+  release(&bcache.bufmap_locks[key]);
 }
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  uint key = BUFMAP_HASH(b->dev, b->blockno);
+  acquire(&bcache.bufmap_locks[key]);
   b->refcnt++;
-  release(&bcache.lock);
+  release(&bcache.bufmap_locks[key]);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  uint key = BUFMAP_HASH(b->dev, b->blockno);
+  acquire(&bcache.bufmap_locks[key]);
   b->refcnt--;
-  release(&bcache.lock);
+  release(&bcache.bufmap_locks[key]);
 }
 
 
--- kernel/buf.h
+++ kernel/buf.h
@@ -1,12 +1,14 @@
 struct buf {
   int valid;   // has data been read from disk?
   int disk;    // does disk "own" buf?
-  uint dev;
-  uint blockno;
+  uint dev;     // 设备编号
+  uint blockno;   // 磁盘块号
   struct sleeplock lock;
-  uint refcnt;
-  struct buf *prev; // LRU cache list
+  uint refcnt;    // 引用计数
+  // struct buf *prev; // LRU cache list
   struct buf *next;
   uchar data[BSIZE];
+
+  uint lastuse;   // 该缓冲区的最后使用时间（通常用于实现 LRU 策略）
 };
 
